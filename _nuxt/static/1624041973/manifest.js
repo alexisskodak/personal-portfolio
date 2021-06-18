174202f0d555b8651ee2929368ef47ddde0190e1
sickb0i <skodak95alexis@gmail.com>
2021-06-18 21:15:33 +0200

    83fda59

---
+++ _nuxt/static/1624041973/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Ffr\u002Fcontact","\u002Ffr\u002Fwork","\u002Fabout","\u002Fwork","\u002Ffr","\u002Ffr\u002Fabout","\u002Fcontact","\u002Ffr\u002Fgallery","\u002Fgallery"]})
